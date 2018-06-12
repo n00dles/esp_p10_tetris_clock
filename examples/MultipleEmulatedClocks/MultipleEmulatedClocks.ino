a390395a18f3eb114681b1137ef6d5358f11b1d2
witnessmenow <ecenuig@gmail.com>
2018-06-12 23:06:52 +0100

    Initial version, still wIP

---
+++ examples/MultipleEmulatedClocks/MultipleEmulatedClocks.ino
@@ -0,0 +1,126 @@
+/*******************************************************************
+    Using a 64 * 32 RGB Matrix to display a mario image
+ *                                                                 *
+    Written by Brian Lough
+    https://www.youtube.com/channel/UCezJOfu7OtqGzd5xrP3q6WA
+ *******************************************************************/
+
+// ----------------------------
+// Standard Libraries - Already Installed if you have ESP8266 set up
+// ----------------------------
+
+#include <Ticker.h>
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+
+#include <PxMatrix.h>
+// The library for controlling the LED Matrix
+// Needs to be manually downloaded and installed
+// https://github.com/2dom/PxMatrix
+
+#include <TetrisMatrixDraw.h>
+
+
+Ticker display_ticker;
+Ticker timer_ticker;
+
+// Pins for LED MATRIX
+#define P_LAT 16
+#define P_A 5
+#define P_B 4
+#define P_C 15
+#define P_OE 2
+#define P_D 12
+#define P_E 0
+
+// PxMATRIX display(32,16,P_LAT, P_OE,P_A,P_B,P_C);
+// PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
+PxMATRIX display(64, 32, P_LAT, P_OE, P_A, P_B, P_C, P_D, P_E);
+
+// Some standard colors
+uint16_t myRED = display.color565(255, 0, 0);
+uint16_t myGREEN = display.color565(0, 255, 0);
+uint16_t myBLUE = display.color565(0, 0, 255);
+uint16_t myWHITE = display.color565(255, 255, 255);
+uint16_t myYELLOW = display.color565(255, 255, 0);
+uint16_t myCYAN = display.color565(0, 255, 255);
+uint16_t myMAGENTA = display.color565(255, 0, 255);
+uint16_t myBLACK = display.color565(0, 0, 0);
+
+uint16 myCOLORS[8] = {myRED, myGREEN, myBLUE, myWHITE, myYELLOW, myCYAN, myMAGENTA, myBLACK};
+
+TetrisMatrixDraw tetris(display);
+TetrisMatrixDraw tetrisTwo(display);
+TetrisMatrixDraw tetrisThree(display);
+TetrisMatrixDraw tetrisFour(display);
+
+bool showColon = true;
+
+// ISR for display refresh
+void display_updater()
+{
+
+  display.display(70);
+
+}
+
+void number_updater()
+{
+  display.clearDisplay();
+  tetris.drawNumbers(0,0, showColon);
+  tetrisTwo.drawNumbers(0,16, showColon);
+  tetrisThree.drawNumbers(32,0, showColon);
+  tetrisFour.drawNumbers(32,16, showColon);
+}
+
+void drawIntro(int x = 0, int y = 0)
+{
+  tetris.drawChar("T", x, y, myCYAN);
+  tetris.drawChar("e", x + 5, y, myMAGENTA);
+  tetris.drawChar("t", x + 11, y, myYELLOW);
+  tetris.drawChar("r", x + 17, y, myGREEN);
+  tetris.drawChar("i", x + 22, y, myBLUE);
+  tetris.drawChar("s", x + 27, y, myRED);
+
+  tetris.drawChar("T", x + 6, y + 9, myRED);
+  tetris.drawChar("i", x + 11, y + 9, myWHITE);
+  tetris.drawChar("m", x + 16, y + 9, myCYAN);
+  tetris.drawChar("e", x + 22, y + 9, myMAGENTA);
+}
+
+void setup() {
+  Serial.begin(115200);
+  display.begin(16);
+  display.clearDisplay();
+
+  display_ticker.attach(0.002, display_updater);
+  yield();
+  display.clearDisplay();
+
+  drawIntro(10, 10);
+
+  tetris.setTime("12:59");
+  tetrisTwo.setTime("23:59");
+  tetrisThree.setTime("03:59");
+  tetrisFour.setTime("09:59");
+
+  delay(2000);
+  timer_ticker.attach(0.1, number_updater);
+
+  // Showing how the colon can be flashed from the sketch
+  delay(5000);
+  showColon = false;
+  delay(5000);
+  showColon = true;
+  delay(5000);
+
+  // Shows how it only updates what it needs to update.
+  tetris.setTime("12:56");
+}
+
+
+void loop() {
+}
---
+++ src/TetrisMatrixDraw.cpp
@@ -0,0 +1,337 @@
+/*
+Copyright (c) 2018 Tobias Blum . All right reserved.
+
+Tetris Matrix Clock
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+
+#include "TetrisMatrixDraw.h"
+#include "numbers.h"
+
+TetrisMatrixDraw::TetrisMatrixDraw(PxMATRIX &display)	{
+    this->display = &display;
+    intialiseColors();
+}
+
+void TetrisMatrixDraw::drawChar(String letter, uint8_t x, uint8_t y, uint16_t color)
+{
+    this->display->setTextColor(color);
+    this->display->setCursor(x, y);
+    this->display->print(letter);
+}
+
+// *********************************************************************
+// Draws a brick shape at a given position
+// *********************************************************************
+void TetrisMatrixDraw::drawShape(int blocktype, uint16_t color, int x_pos, int y_pos, int num_rot)
+{
+  // Square
+  if (blocktype == 0)
+  {
+    this->display->drawPixel(x_pos, y_pos, color);
+    this->display->drawPixel(x_pos + 1, y_pos, color);
+    this->display->drawPixel(x_pos, y_pos - 1, color);
+    this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+  }
+
+  // L-Shape
+  if (blocktype == 1)
+  {
+    if (num_rot == 0)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos, y_pos - 2, color);
+    }
+    if (num_rot == 1)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 2, y_pos - 1, color);
+    }
+    if (num_rot == 2)
+    {
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 2, color);
+      this->display->drawPixel(x_pos, y_pos - 2, color);
+    }
+    if (num_rot == 3)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 2, y_pos, color);
+      this->display->drawPixel(x_pos + 2, y_pos - 1, color);
+    }
+  }
+
+  // L-Shape (reverse)
+  if (blocktype == 2)
+  {
+    if (num_rot == 0)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 2, color);
+    }
+    if (num_rot == 1)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 2, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+    }
+    if (num_rot == 2)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos, y_pos - 2, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 2, color);
+    }
+    if (num_rot == 3)
+    {
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 2, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 2, y_pos, color);
+    }
+  }
+
+  // I-Shape
+  if (blocktype == 3)
+  {
+    if (num_rot == 0 || num_rot == 2)
+    { // Horizontal
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 2, y_pos, color);
+      this->display->drawPixel(x_pos + 3, y_pos, color);
+    }
+    if (num_rot == 1 || num_rot == 3)
+    { // Vertical
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos, y_pos - 2, color);
+      this->display->drawPixel(x_pos, y_pos - 3, color);
+    }
+  }
+
+  // S-Shape
+  if (blocktype == 4)
+  {
+    if (num_rot == 0 || num_rot == 2)
+    {
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos, y_pos - 2, color);
+    }
+    if (num_rot == 1 || num_rot == 3)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 2, y_pos - 1, color);
+    }
+  }
+
+  // S-Shape (reversed)
+  if (blocktype == 5)
+  {
+    if (num_rot == 0 || num_rot == 2)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 2, color);
+    }
+    if (num_rot == 1 || num_rot == 3)
+    {
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 2, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+    }
+  }
+
+  // Half cross
+  if (blocktype == 6)
+  {
+    if (num_rot == 0)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos + 2, y_pos, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+    }
+    if (num_rot == 1)
+    {
+      this->display->drawPixel(x_pos, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos, y_pos - 2, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+    }
+    if (num_rot == 2)
+    {
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 2, y_pos - 1, color);
+    }
+    if (num_rot == 3)
+    {
+      this->display->drawPixel(x_pos + 1, y_pos, color);
+      this->display->drawPixel(x_pos, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 1, color);
+      this->display->drawPixel(x_pos + 1, y_pos - 2, color);
+    }
+  }
+}
+
+void TetrisMatrixDraw::setNumState(int index, int value, int x_shift)
+{
+    if(index < MAX_NUMBERS) {
+      Serial.println(value);
+        this->numstates[index].num_to_draw = value;
+        this->numstates[index].x_shift = x_shift;
+        this->numstates[index].fallindex = 0;
+        this->numstates[index].blockindex = 0;
+    }
+}
+
+void TetrisMatrixDraw::setTime(String time)
+{
+    this->sizeOfValue = 4;
+    time.replace(":", "");
+    for (uint8_t pos = 0; pos < 4; pos++)
+    {
+      int number = time.substring(pos, pos + 1).toInt();
+      if (number != this->numstates[pos].num_to_draw)
+      {
+        setNumState(pos, number, xShiftClock[pos]);
+      }
+    }
+}
+
+void TetrisMatrixDraw::drawNumbers(int x, int y, bool displayColon)
+{
+  // For each number position
+  for (int numpos = 0; numpos < this->sizeOfValue; numpos++)
+  {
+
+    // Draw falling shape
+    if (numstates[numpos].blockindex < blocksPerNumber[numstates[numpos].num_to_draw])
+    {
+      fall_instr current_fall = getFallinstrByNum(numstates[numpos].num_to_draw, numstates[numpos].blockindex);
+
+      // Handle variations of rotations
+      uint8_t rotations = current_fall.num_rot;
+      if (rotations == 1)
+      {
+        if (numstates[numpos].fallindex < (int)(current_fall.y_stop / 2))
+        {
+          rotations = 0;
+        }
+      }
+      if (rotations == 2)
+      {
+        if (numstates[numpos].fallindex < (int)(current_fall.y_stop / 3))
+        {
+          rotations = 0;
+        }
+        if (numstates[numpos].fallindex < (int)(current_fall.y_stop / 3 * 2))
+        {
+          rotations = 1;
+        }
+      }
+      if (rotations == 3)
+      {
+        if (numstates[numpos].fallindex < (int)(current_fall.y_stop / 4))
+        {
+          rotations = 0;
+        }
+        if (numstates[numpos].fallindex < (int)(current_fall.y_stop / 4 * 2))
+        {
+          rotations = 1;
+        }
+        if (numstates[numpos].fallindex < (int)(current_fall.y_stop / 4 * 3))
+        {
+          rotations = 2;
+        }
+      }
+
+      drawShape(current_fall.blocktype, this->tetrisColors[current_fall.color], x + current_fall.x_pos + numstates[numpos].x_shift, y + numstates[numpos].fallindex - 1, rotations);
+      numstates[numpos].fallindex++;
+
+      if (numstates[numpos].fallindex > current_fall.y_stop)
+      {
+        numstates[numpos].fallindex = 0;
+        numstates[numpos].blockindex++;
+      }
+    }
+
+    // Draw already dropped shapes
+    if (numstates[numpos].blockindex > 0)
+    {
+      for (int i = 0; i < numstates[numpos].blockindex; i++)
+      {
+        fall_instr fallen_block = getFallinstrByNum(numstates[numpos].num_to_draw, i);
+        drawShape(fallen_block.blocktype, this->tetrisColors[fallen_block.color], x + fallen_block.x_pos + numstates[numpos].x_shift, y + fallen_block.y_stop - 1, fallen_block.num_rot);
+      }
+    }
+  }
+
+  // Hour / minutes divider (blinking)
+  // if (seconds_odd)
+  if (displayColon)
+  {
+    display->fillRect(x + 14, y + 12, 2, 2, this->tetrisWHITE);
+    display->fillRect(x + 14, y + 8, 2, 2, this->tetrisWHITE);
+  }
+}
+
+void TetrisMatrixDraw::intialiseColors(){
+    this->tetrisRED = this->display->color565(255, 0, 0);
+    this->tetrisGREEN = this->display->color565(0, 255, 0);
+    this->tetrisBLUE = this->display->color565(48, 73, 255);
+    this->tetrisWHITE = this->display->color565(255, 255, 255);
+    this->tetrisYELLOW = this->display->color565(255, 255, 0);
+    this->tetrisCYAN = this->display->color565(0, 255, 255);
+    this->tetrisMAGENTA = this->display->color565(255, 0, 255);
+    this->tetrisORANGE = this->display->color565(255, 96, 0);
+    this->tetrisBLACK = this->display->color565(0, 0, 0);
+    
+    this->tetrisColors[0] = this->tetrisRED; 
+    this->tetrisColors[1] = this->tetrisGREEN; 
+    this->tetrisColors[2] = this->tetrisBLUE; 
+    this->tetrisColors[3] = this->tetrisWHITE; 
+    this->tetrisColors[4] = this->tetrisYELLOW; 
+    this->tetrisColors[5] = this->tetrisCYAN; 
+    this->tetrisColors[6] = this->tetrisMAGENTA;
+    this->tetrisColors[7] = this->tetrisORANGE; 
+    this->tetrisColors[8] = this->tetrisBLACK;
+}
+
+// void TetrisMatrixDraw::clearNumStates(){
+//     for(int i = 0; i < MAX_NUMBERS; i++){
+//         this->numstates[i]
+//         this->numstates[i].num_to_draw = 0;
+//         this->numstates[i].fallindex = 0;
+//         this->numstates[i].blockindex = 0;
+//     }
+// }
